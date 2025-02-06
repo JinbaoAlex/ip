288af6dbf1b8792fe943858a66c9602e4b32ffae
JinbaoAlex <e1122101@u.nus.edu>
2025-02-06 20:38:51 +0800

    Level-0

---
+++ src/main/java/Volkov.java
@@ -0,0 +1,11 @@
+public class Volkov {
+    public static void main(String[] args) {
+        String logo = "____________________________________________________________\n"
+                + "Hello! I'm Volkov\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.println(logo);
+    }
+}
