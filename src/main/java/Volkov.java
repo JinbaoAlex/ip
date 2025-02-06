df3b9c38aba15d8c26389605de2e2717bd7d3ca2
JinbaoAlex <e1122101@u.nus.edu>
2025-02-06 20:45:22 +0800

    Level-1 Echo

--- src/main/java/Volkov.java
+++ src/main/java/Volkov.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
+
 public class Volkov {
+    public static String basicResponse(String msg) {
+        return "    ____________________________________________________________\n"
+                + "    " + msg + "\n"
+                + "    ____________________________________________________________\n";
+    }
+
     public static void main(String[] args) {
-        String logo = "____________________________________________________________\n"
-                + "Hello! I'm Volkov\n"
-                + "What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________\n";
-        System.out.println(logo);
+        String opening = "    ____________________________________________________________\n"
+                + "    Hello! I'm Volkov\n"
+                + "    What can I do for you?\n"
+                + "    ____________________________________________________________\n";
+        System.out.println(opening);
+
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
+        while(!command.equals("bye")) {
+            String reply = basicResponse(command);
+            System.out.println(reply);
+            command = sc.nextLine();
+        }
+        String end = basicResponse("Bye. Hope to see you again soon!");
+        System.out.println(end);
     }
 }
