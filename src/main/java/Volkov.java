dc2fcfeadf2cbffd9e09881d97bcbb0cf71a3263
JinbaoAlex <e1122101@u.nus.edu>
2025-02-07 09:45:32 +0800

    Level-4 Add support for tracking todos, deadlines, and events as tasks

--- src/main/java/Volkov.java
+++ src/main/java/Volkov.java
@@ -9,6 +9,15 @@ public class Volkov {
                 + "    ____________________________________________________________\n";
     }
 
+    public static String formatTaskMsg(Task t) {
+        listOfTasks[storeTaskIndex] = t;
+        storeTaskIndex++;
+        String msg = "     Got it. I've added this task:\n"
+                + "       " + t.toString() + "\n"
+                + "     Now you have " + storeTaskIndex + " tasks in the list.";
+        return formatResponse(msg);
+    }
+
     public static void main(String[] args) {
         String opening = "    ____________________________________________________________\n"
                 + "     Hello! I'm Volkov\n"
@@ -24,30 +33,56 @@ public class Volkov {
             if (input.equals("bye")) {
                 System.out.println(formatResponse("     Bye. Hope to see you again soon!"));
                 break;
+
             } else if (input.equals("list")) {
                 StringBuilder sb = new StringBuilder();
                 boolean first = true;
+                sb.append("     Here are the tasks in your list:\n");
                 for (int i = 0; i < storeTaskIndex; i++) {
                     if (first) {
                         first = false;
                     } else {
                         sb.append("\n");
                     }
-                    sb.append("    " + listOfTasks[i]);
+                    sb.append("     ").append(i + 1).append(".").append(listOfTasks[i]);
                 }
                 System.out.println(formatResponse(sb.toString()));
+
             } else if (input.startsWith("mark")) {
-                int taskNo = Integer.parseInt(input.substring(5));
+                int taskNo = Integer.parseInt(input.substring(5)) - 1;
                 listOfTasks[taskNo].markAsDone();
                 String reply = "     Nice! I've marked this task as done:\n"
                         + "       " + listOfTasks[taskNo].toString();
                 System.out.println(formatResponse(reply));
+
             } else if (input.startsWith("unmark")) {
                 int taskNo = Integer.parseInt(input.substring(7));
                 listOfTasks[taskNo].unmarkAsDone();
                 String reply = "     OK, I've marked this task as not done yet:\n"
                         + "       " + listOfTasks[taskNo].toString();
                 System.out.println(formatResponse(reply));
+
+            } else if (input.startsWith("todo")) {
+                String taskDesc = input.substring(5);
+                Todo t = new Todo(taskDesc);
+                System.out.println(formatTaskMsg(t));
+
+            } else if (input.startsWith("deadline")) {
+                int indexOfDeadline = input.indexOf(" /by");
+                String taskDesc = input.substring(9, indexOfDeadline);
+                String deadline = input.substring(indexOfDeadline + 4);
+                Deadline t = new Deadline(taskDesc, deadline);
+                System.out.println(formatTaskMsg(t));
+
+            } else if (input.startsWith("event")) {
+                int indexOfStartDate = input.indexOf(" /from");
+                int indexOfEndDate = input.indexOf(" /to");
+                String taskDesc = input.substring(6, indexOfStartDate);
+                String startDate = input.substring(indexOfStartDate + 6, indexOfEndDate);
+                String endDate = input.substring(indexOfEndDate + 4);
+                Event t = new Event(taskDesc, startDate, endDate);
+                System.out.println(formatTaskMsg(t));
+
             } else {
                 String reply = formatResponse("     " + input);
                 System.out.println(reply);
