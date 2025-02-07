9067561fa77357f39aaf606cdf31513386bf20c2
JinbaoAlex <e1122101@u.nus.edu>
2025-02-07 15:29:20 +0800

    improved code quality, added tags to past commits

--- src/main/java/Volkov.java
+++ src/main/java/Volkov.java
@@ -2,7 +2,28 @@ import java.util.Scanner;
 
 public class Volkov {
     private static Task[] listOfTasks = new Task[100];
-    private static int storeTaskIndex = 0;
+    private static int listOfTasksIndex = 0;
+
+    private static final int MARK_CMD_LENGTH = 5;
+    private static final int UNMARK_CMD_LENGTH = 7;
+    private static final int TODO_CMD_LENGTH = 5;
+    private static final int DEADLINE_CMD_LENGTH = 9;
+    private static final int EVENT_CMD_LENGTH = 6;
+    private static final int DEADLINE_BY_LENGTH = 4;
+    private static final int EVENT_FROM_LENGTH = 6;
+    private static final int EVENT_TO_LENGTH = 4;
+
+    private static final String BYE_COMMAND = "bye";
+    private static final String LIST_COMMAND = "list";
+    private static final String MARK_COMMAND = "mark";
+    private static final String UNMARK_COMMAND = "unmark";
+    private static final String TODO_COMMAND = "todo";
+    private static final String DEADLINE_COMMAND = "deadline";
+    private static final String EVENT_COMMAND = "event";
+    private static final String BY_KEYWORD = " /by";
+    private static final String FROM_KEYWORD = " /from";
+    private static final String TO_KEYWORD = " /to";
+
     public static String formatResponse(String msg) {
         return "    ____________________________________________________________\n"
                 + msg + "\n"
@@ -10,12 +31,65 @@ public class Volkov {
     }
 
     public static String formatTaskMsg(Task t) {
-        listOfTasks[storeTaskIndex] = t;
-        storeTaskIndex++;
+        listOfTasks[listOfTasksIndex] = t;
+        listOfTasksIndex++;
         String msg = "     Got it. I've added this task:\n"
                 + "       " + t.toString() + "\n"
-                + "     Now you have " + storeTaskIndex + " tasks in the list.";
+                + "     Now you have " + listOfTasksIndex + " tasks in the list.";
         return formatResponse(msg);
+    }
+
+    public static void doByeCommand() {
+        System.out.println(formatResponse("     Bye. Hope to see you again soon!"));
+    }
+
+    public static void doListCommand() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("     Here are the tasks in your list:");
+        for (int i = 0; i < listOfTasksIndex; i++) {
+            sb.append("\n     ").append(i + 1).append(".").append(listOfTasks[i]);
+        }
+        System.out.println(formatResponse(sb.toString()));
+    }
+
+    public static void doMarkCommand(String input) {
+        int taskNo = Integer.parseInt(input.substring(MARK_CMD_LENGTH)) - 1;
+        listOfTasks[taskNo].markAsDone();
+        String reply = "     Nice! I've marked this task as done:\n"
+                + "       " + listOfTasks[taskNo].toString();
+        System.out.println(formatResponse(reply));
+    }
+
+    public static void doUnmarkCommand(String input) {
+        int taskNo = Integer.parseInt(input.substring(UNMARK_CMD_LENGTH)) - 1;
+        listOfTasks[taskNo].unmarkAsDone();
+        String reply = "     OK, I've marked this task as not done yet:\n"
+                + "       " + listOfTasks[taskNo].toString();
+        System.out.println(formatResponse(reply));
+    }
+
+    public static void doTodoCommand(String input) {
+        String taskDesc = input.substring(TODO_CMD_LENGTH);
+        Todo t = new Todo(taskDesc);
+        System.out.println(formatTaskMsg(t));
+    }
+
+    public static void doDeadlineCommand(String input) {
+        int indexOfDeadline = input.indexOf(BY_KEYWORD);
+        String taskDesc = input.substring(DEADLINE_CMD_LENGTH, indexOfDeadline);
+        String deadline = input.substring(indexOfDeadline + DEADLINE_BY_LENGTH);
+        Deadline t = new Deadline(taskDesc, deadline);
+        System.out.println(formatTaskMsg(t));
+    }
+
+    public static void doEventCommand(String input) {
+        int indexOfStartDate = input.indexOf(FROM_KEYWORD);
+        int indexOfEndDate = input.indexOf(TO_KEYWORD);
+        String taskDesc = input.substring(EVENT_CMD_LENGTH, indexOfStartDate);
+        String startDate = input.substring(indexOfStartDate + EVENT_FROM_LENGTH, indexOfEndDate);
+        String endDate = input.substring(indexOfEndDate + EVENT_TO_LENGTH);
+        Event t = new Event(taskDesc, startDate, endDate);
+        System.out.println(formatTaskMsg(t));
     }
 
     public static void main(String[] args) {
@@ -30,65 +104,34 @@ public class Volkov {
         while (true) {
             String input = sc.nextLine();
 
-            if (input.equals("bye")) {
-                System.out.println(formatResponse("     Bye. Hope to see you again soon!"));
+            if (input.equals(BYE_COMMAND)) {
+                doByeCommand();
                 break;
 
-            } else if (input.equals("list")) {
-                StringBuilder sb = new StringBuilder();
-                boolean first = true;
-                sb.append("     Here are the tasks in your list:\n");
-                for (int i = 0; i < storeTaskIndex; i++) {
-                    if (first) {
-                        first = false;
-                    } else {
-                        sb.append("\n");
-                    }
-                    sb.append("     ").append(i + 1).append(".").append(listOfTasks[i]);
-                }
-                System.out.println(formatResponse(sb.toString()));
+            } else if (input.equals(LIST_COMMAND)) {
+                doListCommand();
 
-            } else if (input.startsWith("mark")) {
-                int taskNo = Integer.parseInt(input.substring(5)) - 1;
-                listOfTasks[taskNo].markAsDone();
-                String reply = "     Nice! I've marked this task as done:\n"
-                        + "       " + listOfTasks[taskNo].toString();
-                System.out.println(formatResponse(reply));
+            } else if (input.startsWith(MARK_COMMAND)) {
+                doMarkCommand(input);
 
-            } else if (input.startsWith("unmark")) {
-                int taskNo = Integer.parseInt(input.substring(7));
-                listOfTasks[taskNo].unmarkAsDone();
-                String reply = "     OK, I've marked this task as not done yet:\n"
-                        + "       " + listOfTasks[taskNo].toString();
-                System.out.println(formatResponse(reply));
+            } else if (input.startsWith(UNMARK_COMMAND)) {
+                doUnmarkCommand(input);
 
-            } else if (input.startsWith("todo")) {
-                String taskDesc = input.substring(5);
-                Todo t = new Todo(taskDesc);
-                System.out.println(formatTaskMsg(t));
+            } else if (input.startsWith(TODO_COMMAND)) {
+                doTodoCommand(input);
 
-            } else if (input.startsWith("deadline")) {
-                int indexOfDeadline = input.indexOf(" /by");
-                String taskDesc = input.substring(9, indexOfDeadline);
-                String deadline = input.substring(indexOfDeadline + 4);
-                Deadline t = new Deadline(taskDesc, deadline);
-                System.out.println(formatTaskMsg(t));
+            } else if (input.startsWith(DEADLINE_COMMAND)) {
+                doDeadlineCommand(input);
 
-            } else if (input.startsWith("event")) {
-                int indexOfStartDate = input.indexOf(" /from");
-                int indexOfEndDate = input.indexOf(" /to");
-                String taskDesc = input.substring(6, indexOfStartDate);
-                String startDate = input.substring(indexOfStartDate + 6, indexOfEndDate);
-                String endDate = input.substring(indexOfEndDate + 4);
-                Event t = new Event(taskDesc, startDate, endDate);
-                System.out.println(formatTaskMsg(t));
+            } else if (input.startsWith(EVENT_COMMAND)) {
+                doEventCommand(input);
 
             } else {
                 String reply = formatResponse("     " + input);
                 System.out.println(reply);
                 Task t = new Task(input);
-                listOfTasks[storeTaskIndex] = t;
-                storeTaskIndex++;
+                listOfTasks[listOfTasksIndex] = t;
+                listOfTasksIndex++;
             }
         }
     }
